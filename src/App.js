f5312b34d94e2be784122b57ee60ed208436ecad
mosband <mosband@163.com>
2020-05-21 20:48:26 +0800

    antd按需加载&自定义主题

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
+import { Button, message } from "antd";
 
-function App() {
-  return <div className="App">React Admin</div>;
+/* 
+应用的根组件
+*/
+
+export default class App extends Component {
+  handleClick = () => {
+    message.success("成功啦");
+  };
+  render() {
+    return (
+      <Button type="primary" onClick={this.handleClick}>
+        测试antd
+      </Button>
+    );
+  }
 }
-
-export default App;
