b92444fd5ce40c5ad0f24fc08e9d9b34c20de5be
mosband <mosband@163.com>
2020-05-27 18:05:49 +0800

    退出功能&时间

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
 import { pathRouteMap } from "@/config/routes.admin.js";
+import { Button, Modal } from "antd";
+import moment from "moment";
 import style from "./index.module.less";
 class Header extends Component {
   state = {
     city: "",
     wea: "",
-    tem: ""
+    tem: "",
+    time: ""
   };
   getTitle = () => {
     const path = this.props.location.pathname;
@@ -14,7 +17,17 @@ class Header extends Component {
     return (route && route.title) || "";
   };
 
-  UNSAFE_componentWillMount() {
+  logout = () => {
+    Modal.confirm({
+      content: "确定要退出吗？",
+      onOk: () => {
+        React.$removeUser();
+        this.props.history.replace("/login");
+      }
+    });
+  };
+
+  getWeather = () => {
     React.$jsonp(
       "https://www.tianqiapi.com/free/day?appid=23035354&appsecret=8YvlPNrz"
     ).then(res => {
@@ -24,13 +37,38 @@ class Header extends Component {
         tem: res.tem
       });
     });
+  };
+
+  getTime = () => {
+    this.timeInterval = setInterval(() => {
+      this.setState({
+        time: moment().format("MMMM Do YYYY, h:mm:ss a")
+      });
+    }, 1000);
+  };
+
+  componentDidMount() {
+    this.getWeather();
+    this.getTime();
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timeInterval);
   }
   render() {
     const user = React.$getUser();
     const title = this.getTitle();
     return (
       <div className={style.header}>
-        <div className={style.top}>{user.username}</div>
+        <div className={style.top}>
+          <span className={style.left}>{this.state.time}</span>
+          <span className={style.right}>
+            <span>{user.username}</span>
+            <Button type="primary" onClick={this.logout}>
+              退出
+            </Button>
+          </span>
+        </div>
         <div className={style.bottom}>
           <span className={style.left}>
             <span className={style.title}>{title}</span>
